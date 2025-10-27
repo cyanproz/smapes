3f4339b7ee17bc9523f01d0671b2d43cba40e70e
CyanProz <alif.pradani@outlook.com>
2025-10-27 15:57:32 +0700

    Some updates

--- public/partials/page-footer.php
+++ public/partials/page-footer.php
@@ -11,15 +11,6 @@
             Temukan kami di sosial media:<br>
             Instagram: @SMAPES
           </div>
-          <!-- <div>
-            Language:<br>
-            <select name="pricing" id="pricing-dropdown" style="color: black;">
-              <optgroup label="Choose a Language">
-                <option value="id">Bahasa Indonesia (Indonesian)</option>
-                <option value="en">English (English)</option>
-              </optgroup>
-            </select>
-          </div> -->
         </div>
         <div style="text-align: center;">
           SMAPES | Copyright &copy; Alif 2025
